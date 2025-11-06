05bc6ad7d09c48ac3e72e01819d532897b6bc771
Igor Lišinski <igor.lisinski@svamplus.hr>
2025-11-06 15:56:15 +0100

    Dorade

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 export const SITE_PREFIX = "/erp";
 
-export const APP_VERSION = "1.6"
+
 //export const SERVICE_DOMAIN = 'http://testoperaweb/testapi2019/api';
 export const SERVICE_DOMAIN = 'http://localhost:42238/api';
 
